cb317b06f6fbf304e8d3f94c70ef70bbd9f88e72
yerin1024 <yerin102@nate.com>
2025-02-27 17:17:05 +0900

    @Scope의 proxyMode옵션을 사용해서 request스코프 테스트 (64강)

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -13,15 +13,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody //문자열 그대로 응답 보냄
     public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestURL = request.getRequestURL().toString();
-
-        //ObjectProvider를 사용한 이유는 생성자주입으로 MyLogger를 받고싶지만 MyLogger가 request스코프이기 때문에 request가 있어야 되는데 logDemo를 생성할 때 request가 없기 때문
-        MyLogger myLogger = myLoggerProvider.getObject();
+        System.out.println("myLogger = "+myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
